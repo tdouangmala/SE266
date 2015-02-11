3b699955a11ae796735b42113c8381b646c354ac
tdouangmala <tdouangmala@email.neit.edu>
2015-02-11 10:06:54 -0500

    Lab 7-1 Completed

--- ex_starts/ch07_ex1/display_results.php
+++ ex_starts/ch07_ex1/display_results.php
@@ -3,12 +3,30 @@
     $email = $_POST['email'];
 
     // get the rest of the data for the form
+	$password = $_POST['password'];
+	$phone = $_POST['phone'];
 
     // for the heard_from radio buttons,
     // display a value of 'Unknown' if the user doesn't select a radio button
+	if (isset($_POST['heard_from'])) {
+		$heard_from = $_POST['heard_from'];
+	} else {
+		$heard_from = "unknown";
+	}
 
     // for the wants_updates check box,
     // display a value of 'Yes' or 'No'
+	if (isset($_POST['wants_updates'])) {
+		$wants_updates = 'YES';
+	} else {
+		$wants_updates = 'NO';
+	}
+
+	$contact_via = $_POST['contact_via'];
+
+	// text area
+	$comments = $_POST['comments'];
+	$comments = nl2br($comments, false);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -25,22 +43,22 @@
         <span><?php echo htmlspecialchars($email); ?></span><br />
 
         <label>Password:</label>
-        <span><!-- add PHP code here--></span><br />
+        <span><?php echo htmlspecialchars($password); ?></span><br />
 
         <label>Phone Number:</label>
-        <span></span><br />
+        <span><?php echo htmlspecialchars($phone); ?></span><br />
 
         <label>Heard From:</label>
-        <span></span><br />
+        <span><?php echo $heard_from; ?></span><br />
 
         <label>Send Updates:</label>
-        <span></span><br />
+        <span><?php echo $wants_updates; ?></span><br />
 
         <label>Contact Via:</label>
-        <span></span><br /><br />
+        <span><?php echo $contact_via; ?></span><br /><br />
 
         <span>Comments:</span><br />
-        <span></span><br />
+        <span><?php echo $comments; ?></span><br />
         
         <p>&nbsp;</p>
     </div>
